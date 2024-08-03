2bdd1f11d4b94a4349e0a31403b0a270cd9b8ff3
amittkulkarni <amitcoolkarni@outlook.com>
2024-08-04 00:04:33 +0530

    Updated Database and added Migrations

---
+++ backend/api/parsers.py
@@ -0,0 +1,2 @@
+#  will be used for parsing request data
+#  can use marshmallow or request parser
---
+++ backend/api/resources.py
@@ -0,0 +1,51 @@
+from flask_restx import Resource
+from api_models import user_model, movie_model, theatre_model, showtime_model, reservation_model
+from backend import api
+from backend.models import User, Movie, Theatre, Showtime, Reservation
+
+
+# ------------------------------------------------------------------------------------
+
+# This is for testing purposes, not final. Will be completed later. Feel free to add.
+
+# ------------------------------------------------------------------------------------
+
+
+@api.route('/users', '/users/<int:id>')
+class UserResource(Resource):
+    @api.marshal_with(user_model)
+    def get(self):
+        user = User.query.filter_by(role='Customer').first()
+        return user
+
+
+@api.route('/movies', '/movies/<int:id>')
+class MovieResource(Resource):
+    @api.marshal_with(movie_model)
+    def get(self):
+        movie = Movie.query.all()
+        return movie
+
+
+@api.route('/theatres', '/theatres/<int:id>')
+class TheatreResource(Resource):
+    @api.marshal_with(theatre_model)
+    def get(self):
+        theatre = Theatre.query.all()
+        return theatre
+
+
+@api.route('/showtimes', '/showtimes/<int:id>')
+class ShowtimeResource(Resource):
+    @api.marshal_with(showtime_model)
+    def get(self):
+        showtime = Showtime.query.all()
+        return showtime
+
+
+@api.route('/reservations/<int:id>')
+class ReservationResource(Resource):
+    @api.marshal_with(reservation_model)
+    def get(self):
+        reservation = Reservation.query.all()
+        return reservation
